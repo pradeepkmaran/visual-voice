1e4cfb5dff635a3764c321d17b590a8654e34c78
Pradeep KM <pradeepkm6122004@gmail.com>
2024-11-25 19:07:08 +0530

    feat: extension audio check

---
+++ content.js
@@ -0,0 +1,5 @@
+document.getElementById("playAudio").addEventListener("click", () => {
+    const audio = new Audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
+    audio.play();
+});
+  
